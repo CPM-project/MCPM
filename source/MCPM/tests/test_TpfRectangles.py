c25a642e978b6d18bdda8fe30954562959346c0a
Radek Poleski <radek.poleski@gmail.com>
2017-10-26 15:34:53 -0400

    correction in test_closest_epics_1()

--- source/MCPM/tests/test_TpfRectangles.py
+++ source/MCPM/tests/test_TpfRectangles.py
@@ -14,7 +14,7 @@ def test_closest_epics_1():
                 '200071070', '248368847', '200071060', '200071061', '200071116']
     expect_2 = [0.0, 8.0, 20.0, 21.541, 45.0, 45.706, 56.851, 58.0, 58.549, 61.0]
     
-    assert out_1[:10] == expect_1
+    assert (out_1[:10] == expect_1).all()
     np.testing.assert_almost_equal(out_2[:10], expect_2, decimal=3)
     assert len(out_1) == 380
     assert len(rectangle.epic) == 381
