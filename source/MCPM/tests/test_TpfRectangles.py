a9216617be3fc6b65fa383c7e99d010e3dbc2120
Radek Poleski <radek.poleski@gmail.com>
2017-10-27 15:44:32 -0400

    updating tests

--- source/MCPM/tests/test_TpfRectangles.py
+++ source/MCPM/tests/test_TpfRectangles.py
@@ -12,7 +12,7 @@ def test_closest_epics_1():
     
     expect_1 = ['200071074', '200071071', '200071075', '200071072', '200071117', 
                 '200071070', '248368847', '200071060', '200071061', '200071116']
-    expect_2 = [0.0, 8.0, 20.0, 21.541, 45.0, 45.706, 56.851, 58.0, 58.549, 61.0]
+    expect_2 = [0.0, 9., 19., 21.024, 46.0, 46.872, 55.444, 59.0, 59.414, 62.0]
     
     assert (out_1[:10] == expect_1).all()
     np.testing.assert_almost_equal(out_2[:10], expect_2, decimal=3)
@@ -26,4 +26,17 @@ def test_get_epic_id_for_pixel_1():
     rectangle = TpfRectangles(campaign, channel)
 
     assert rectangle.get_epic_id_for_pixel(670, 883) == '200071074'
+    
+def test_get_epic_id_for_pixel_2():    
+    channel = 31
+    campaign = 91 
+    pix_x = 262
+    pix_y = 468
+    epic = '200069761'
+    
+    rectangle = TpfRectangles(campaign, channel)
+
+    assert rectangle.get_epic_id_for_pixel(pix_x, pix_y) == epic
+    assert rectangle.get_epic_id_for_pixel(pix_x-1, pix_y) != epic
+    assert rectangle.get_epic_id_for_pixel(pix_x, pix_y-1) != epic
     
