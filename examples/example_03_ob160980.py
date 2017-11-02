b01ef2d98d31e002567fe00b6ffe029b7a57c7cd
Radek Poleski <radek.poleski@gmail.com>
2017-11-02 13:12:47 -0400

    significant update of example_03

--- examples/example_03_ob160980.py
+++ examples/example_03_ob160980.py
@@ -7,6 +7,7 @@ import sys
 from MCPM import utils
 from MCPM.cpmfitsource import CpmFitSource
 
+
 def pspl_model(t_0, u_0, t_E, f_s, time=None, cpm_source=None):
     """Paczyncki model, provide either time vector or CpmFitSource instance cpm_source"""
     if (time is None) == (cpm_source is None):
@@ -21,11 +22,11 @@ def pspl_model(t_0, u_0, t_E, f_s, time=None, cpm_source=None):
     
     return model
     
-def fun_3(inputs, cpm_source, l2):
+def fun_3(inputs, cpm_source, l2, t_E):
     """3-parameter function for optimisation; t_E - fixed"""
     t_0 = inputs[0]
     u_0 = inputs[1]
-    t_E = 21.
+    t_E = t_E
     f_s = inputs[2]
     
     if u_0 < 0. or t_E < 0. or f_s < 0.:
@@ -35,8 +36,8 @@ def fun_3(inputs, cpm_source, l2):
 
     cpm_source.run_cpm(l2, model)
     
-    print(t_0, u_0, t_E, f_s, cpm_source.residual_rms)
-    return cpm_source.residual_rms
+    #print(t_0, u_0, t_E, f_s, cpm_source.residuals_rms)
+    return cpm_source.residuals_rms
     
 def fun_4(inputs, cpm_source, l2):
     """4-parameter function for optimisation"""
@@ -52,8 +53,8 @@ def fun_4(inputs, cpm_source, l2):
 
     cpm_source.run_cpm(l2, model)
     
-    print(t_0, u_0, t_E, f_s, cpm_source.residual_rms)
-    return cpm_source.residual_rms
+    #print(t_0, u_0, t_E, f_s, cpm_source.residuals_rms)
+    return cpm_source.residuals_rms
     
 
 if __name__ == "__main__":
@@ -66,12 +67,14 @@ if __name__ == "__main__":
     half_size = 2
     n_select = 10
     l2 = 10**8.5
-    l2 = 10**8
+    #l2 = 10**6
     start = np.array([7556., 0.14, 21., 300.])
-    start_3 = np.array([7556., 0.1, 105.])
-    tol = 0.0001
+    start_3 = np.array([7556., .1, 150.])
+    t_E = 21.
+    tol = 0.001
     #method = 'Nelder-Mead' # only these 2 make sense
     method = 'Powell'
+    n_remove = 10
     
     cpm_source = CpmFitSource(ra=ra, dec=dec, campaign=campaign, 
             channel=channel)
@@ -80,36 +83,60 @@ if __name__ == "__main__":
     cpm_source.set_pixels_square(half_size)
     cpm_source.select_highest_prf_sum_pixels(n_select)
 
-    # Optimize model parameters:"
-    args = (cpm_source, l2)
+    # Optimize model parameters:
+    args = (cpm_source, l2, t_E)
     out = minimize(fun_3, start_3, args=args, tol=tol, method=method)
-    print()  
-    print(out.success)
     print(out)
     
-    model = pspl_model(out.x[0], out.x[1], 21., out.x[2], cpm_source=cpm_source)
+    # plot the best model
+    model = pspl_model(out.x[0], out.x[1], t_E, out.x[2], cpm_source=cpm_source)
     cpm_source.run_cpm(l2, model)
-    mask = cpm_source.residue_mask
-    plt.plot(cpm_source.pixel_time[mask], cpm_source.residue[mask]+model[mask], '.')
-    plt.plot(cpm_source.pixel_time[mask], cpm_source.residue[mask], '.')
+    print("RMS: {:.4f}  {:}".format(cpm_source.residuals_rms, np.sum(cpm_source.residuals_mask)))
+    mask = cpm_source.residuals_mask
+    plt.plot(cpm_source.pixel_time[mask], cpm_source.residuals[mask]+model[mask], '.')
+    plt.plot(cpm_source.pixel_time[mask], cpm_source.residuals[mask], '.')
     plt.show()
-    import sys
-    sys.exit()
+    
+    # you may want to plot residuals as a function of position:
+    if False:
+        plt.scatter(cpm_source.x_positions[mask], cpm_source.y_positions[mask], c=np.abs(cpm_source.residuals[mask]))
+        plt.show()
+    
+    # Remove most outlying points:
+    if True:
+        limit = np.sort(np.abs(cpm_source.residuals[mask]))[-n_remove]
+        cpm_source.mask_bad_epochs_residuals(limit)
+        model = pspl_model(out.x[0], out.x[1], t_E, out.x[2], cpm_source=cpm_source)
+        cpm_source.run_cpm(l2, model)
+        print("RMS: {:.4f}  {:}".format(cpm_source.residuals_rms, np.sum(cpm_source.residuals_mask)))
 
-    # Optimize model parameters:"
-    args = (cpm_source, l2)
-    out = minimize(fun_4, start, args=args, tol=tol, method=method)
-    print()  
-    print(out.success)
-    print(out)
+    # Optimize model parameters once more:
+    if True:
+        out = minimize(fun_3, out.x, args=args, tol=tol, method=method)
+        print(out)
+        
+        model = pspl_model(out.x[0], out.x[1], t_E, out.x[2], cpm_source=cpm_source)
+        cpm_source.run_cpm(l2, model)
+        print("RMS: {:.4f}  {:}".format(cpm_source.residuals_rms, np.sum(cpm_source.residuals_mask)))
+        
+        # plot it:
+        if True:
+            mask = cpm_source.residuals_mask
+            plt.plot(cpm_source.pixel_time[mask], cpm_source.residuals[mask]+model[mask], '.')
+            plt.plot(cpm_source.pixel_time[mask], cpm_source.residuals[mask], '.')
+            plt.show()    
+
+    # Optimize model parameters with 4 fitted parameters:
+    if False:
+        args = (cpm_source, l2)
+        out = minimize(fun_4, start, args=args, tol=tol, method=method)
+        print(out)
     #print(out.nfev)
     #print("{:.5f} {:.4f} {:.4f}  ==> {:.4f}".format(out.x[0], out.x[1], out.x[2], out.fun))
     
     #model = transform_model(out.x[0], out.x[1], out.x[2], model_dt, model_flux, cpm_source.pixel_time)
-    model = pspl_model(out.x[0], out.x[1], out.x[2], out.x[3], cpm_source=cpm_source)
-    cpm_source.run_cpm(l2, model)
-    mask = cpm_source.residue_mask
-    plt.plot(cpm_source.pixel_time[mask], cpm_source.residue[mask]+model[mask], '.')
-    plt.show()
-    
-    
+        model = pspl_model(out.x[0], out.x[1], out.x[2], out.x[3], cpm_source=cpm_source)
+        cpm_source.run_cpm(l2, model)
+        mask = cpm_source.residuals_mask
+        plt.plot(cpm_source.pixel_time[mask], cpm_source.residuals[mask]+model[mask], '.')
+        plt.show()
