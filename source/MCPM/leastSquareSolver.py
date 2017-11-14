b0aa8ba39065d015c969862ded28fef5105464a0
Radek Poleski <radek.poleski@gmail.com>
2017-11-14 11:52:02 -0500

    faster decomposition

--- source/MCPM/leastSquareSolver.py
+++ source/MCPM/leastSquareSolver.py
@@ -26,8 +26,30 @@ def linear_least_squares(A, y, yvar=None, l2=None):
         ``A.shape[1]``).
         
     """
+    # Check if A, l2, and yvar are the same as before, if so, then use 
+    # previously remembered factor.
+    if linear_least_squares.A is not None:
+        if (linear_least_squares.A.shape == A.shape
+                    and linear_least_squares.l2 == l2
+                    and linear_least_squares.yvar == yvar):
+            if (linear_least_squares.A == A).all():
+                if len(y.shape)==1:
+                    y = y[:, None]
+                if yvar is not None:
+                    Ciy = y / yvar[:, None]
+                else:
+                    Ciy = y
+                fac = linear_least_squares.factor
+                AT = linear_least_squares.AT
+                return linalg.cho_solve(fac, np.dot(AT, Ciy), overwrite_b=True)
+    
+    linear_least_squares.l2 = l2
+    linear_least_squares.yvar = yvar
+    linear_least_squares.A = A
+    
     if len(y.shape)==1:
         y = y[:, None]
+        
     # Incorporate the observational uncertainties.
     if yvar is not None:
         CiA = A / yvar[:, None]
@@ -38,6 +60,7 @@ def linear_least_squares(A, y, yvar=None, l2=None):
     
     # Compute the pre-factor.
     AT = A.T
+    linear_least_squares.AT = AT    
     ATA = np.dot(AT, CiA)
     
     # Incorporate any L2 regularization.
@@ -48,4 +71,7 @@ def linear_least_squares(A, y, yvar=None, l2=None):
     
     # Solve the equations overwriting the temporary arrays for speed.
     factor = linalg.cho_factor(ATA, overwrite_a=True)
+    linear_least_squares.factor = factor
     return linalg.cho_solve(factor, np.dot(AT, Ciy), overwrite_b=True)
+    
+linear_least_squares.A = None
