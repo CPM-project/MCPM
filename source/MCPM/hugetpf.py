752b1517ebcce24670351854d9df1227747fbcb1
Radek Poleski <radek.poleski@gmail.com>
2018-02-19 12:35:58 -0500

    C11 in hugetpf

--- source/MCPM/hugetpf.py
+++ source/MCPM/hugetpf.py
@@ -1,9 +1,15 @@
 class HugeTpf(object):
     """keeps ids of the largest TPF files; one typically removes the largest 
     files to limit the usage of RAM memory"""
-    def __init__(self, n_huge=2, campaign=91):
+    def __init__(self, n_huge=None, campaign=91):
         if campaign in [91, 92]:
             list_huge = ['200070438', '200070874', '200069673', '200071158']
+            if n_huge is None:
+                n_huge = 2
+        elif campaign in [111, 112]:
+            list_huge = ['235785614']
+            if n_huge is None:
+                n_huge = 0
         else:
             raise ValueError('Campaign {:} not yet coded in HugeTpf.__init__()'.format(campaign))
         if n_huge > len(list_huge):
