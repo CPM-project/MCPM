9e503ae0b97208c3b0e3806489bf397a94015d30
Radek Poleski <radek.poleski@gmail.com>
2018-07-02 19:12:33 -0400

    plot_tpf_pixel_curves.py updated

--- examples/plot_tpf_pixel_curves.py
+++ examples/plot_tpf_pixel_curves.py
@@ -17,7 +17,8 @@ def plot_tpf_data(ra, dec, channel, campaign, file_out, half_size=2, stars_subtr
 
 if __name__ == "__main__":
     
-    stars_0241 = [[270.63370, -27.52653, 30.e3]]
+    #stars_0241 = [[270.63370, -27.52653, 30.e3]]
+    stars_0241 = [[270.63370, -27.52653, 16996.5]]
     plot_tpf_data(270.6323333, -27.5296111, 49, 91, "ob160241_c91_pixel_curves.png", 
         half_size=3, stars_subtract=stars_0241)
     plot_tpf_data(270.6323333, -27.5296111, 49, 92, "ob160241_c92_pixel_curves.png", 
@@ -27,3 +28,5 @@ if __name__ == "__main__":
     plot_tpf_data(269.5648750, -27.9635833, 31, 92, "ob160940_pixel_curves.png")
     plot_tpf_data(271.2375417, -28.6278056, 52, 92, "ob160975_pixel_curves.png")
     plot_tpf_data(271.354292, -28.005583, 52, 92, "ob160980_pixel_curves.png")
+    
+    plot_tpf_data(269.9291250, -28.4108333, 31, 91, "eb234840_pixel_curves.png")
