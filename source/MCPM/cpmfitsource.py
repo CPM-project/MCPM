corrected chi2 calculation

source/MCPM/cpmfitsource.py
@@ -60,6 +60,7 @@ class CpmFitSource(object):
         self._pixel_time = None
         self._pixel_flux = None
         self._pixel_flux_err = None
+        self._all_pixels_flux_err = None
         self._pixel_mask = None  
         self._train_mask = None
         self._cpm_pixel = None
@@ -317,6 +318,9 @@ class CpmFitSource(object):
         self._pixel_flux_err = out[2]
         self._pixel_mask = out[3]
 
+        errors = np.array(self._pixel_flux_err)
+        self._all_pixels_flux_err = np.sqrt(np.sum(errors**2, axis=0))
+
     @property
     def pixel_time(self):
         """time vectors for all pixels"""
@@ -337,7 +341,17 @@ class CpmFitSource(object):
         if self._pixel_flux_err is None:
             self._get_time_flux_mask_for_pixels()
         return self._pixel_flux_err
-        
+
+    @property
+    def all_pixels_flux_err(self):
+        """
+        uncertainty of combined flux of the few pixels that are
+        combined into a source
+        """
+        if self._all_pixels_flux_err is None:
+            self._get_time_flux_mask_for_pixels()
+        return self._all_pixels_flux_err
+
     @property
     def pixel_mask(self):
         """epoch masks for pixel_flux and pixel_time"""

source/MCPM/minimizer.py
@@ -71,6 +71,8 @@ class Minimizer(object):
         self._coeffs_cache = None
         self.n_flush = None
 
+        self.sigma_scale = 1.
+
     def close_file_all_models(self):
         """closes the file to which all models are saved"""
         self._file_all_models.close()
@@ -231,8 +233,14 @@ class Minimizer(object):
         """for a given set of parameters (theta), return the chi2"""
         self._run_cpm(theta)
         n = self.n_datasets - self.n_sat
+        chi2_sat = []
+        for source in self.cpm_sources:
+            residuals = source.residuals[source.residuals_mask]
+            sigma = source.all_pixels_flux_err[source.residuals_mask]
+            sigma *= self.sigma_scale
+            chi2_sat.append(np.sum((residuals/sigma)**2))
         # Correct the line below.
-        chi2_sat = [np.sum(self._sat_masks[i])*(self.cpm_sources[i].residuals_rms/np.mean(self.event.datasets[n+i].err_flux))**2 for i in range(self.n_sat)]
+        #chi2_sat = [np.sum(self._sat_masks[i])*(self.cpm_sources[i].residuals_rms/np.mean(self.event.datasets[n+i].err_flux))**2 for i in range(self.n_sat)]
         # We also tried:
         #chi2_sat = 0.
         #for i in range(self.n_sat):

source/MCPM/minimizervariablestar.py
@@ -163,10 +163,9 @@ class MinimizerVariableStar(object):
         """for a given set of parameters (theta), return the chi2"""
         self._run_cpm(theta)
         self.chi2 = []
-        for i in range(self.n_sat):
-            source = self.cpm_sources[i]
+        for source in self.cpm_sources:
             residuals = source.residuals[source.residuals_mask]
-            sigma = np.sqrt(np.sum(np.array([err[source.residuals_mask] for err in source.pixel_flux_err])**2, axis=0))
+            sigma = source.all_pixels_flux_err[source.residuals_mask]
             sigma *= self.sigma_scale
             self.chi2.append(np.sum((residuals/sigma)**2))
         if self._flux_constraint is not None: