830045d3138924d6dbefd10bc8718b60d9895e1e
Radek Poleski <radek.poleski@gmail.com>
2019-08-19 20:26:04 +0000

    starting cpmasteroid.py

---
+++ source/MCPM/cpmasteroid.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from MCPM.multipletpf import MultipleTpf
+from MCPM.campaigngridradec2pix import CampaignGridRaDec2Pix
+from MCPM.prfdata import PrfData
+from MCPM.prfforcampaign import PrfForCampaign
+
+
+class CpmAsteroid(object):
+    """
+    XXX
+    """
+    
+    def __init__(self, epochs, ra_deg, dec_deg):
+        pass
+
